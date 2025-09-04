e7298c3217fa7e7ae748b54086f4c9ebee35b473
Riley Chang <riley@solitango.com>
2025-09-04 19:32:02 +0800

    reuse existing implement

--- nix/stateless/preset/lib/mkPreset.nix
+++ nix/stateless/preset/lib/mkPreset.nix
@@ -2,17 +2,9 @@
   checkOptionAccessor ? _: true,
   persistentDirectories ? [],
   persistentFiles ? [],
-}: {
-  config,
-  lib,
-  ...
-}: let
-  enabled = config.solitango.stateless.enable && checkOptionAccessor config;
-  persistentStorageMountpoint = config.solitango.stateless.persistentStorageMountpoint;
-in
-  lib.mkIf enabled {
-    environment.persistence."${persistentStorageMountpoint}" = {
-      directories = persistentDirectories;
-      files = persistentFiles;
-    };
-  }
+}:
+(import ./mkDynamicPreset.nix) {
+  checkOptionAccessor = checkOptionAccessor;
+  persistentDirectoriesAccessor = _: persistentDirectories;
+  persistentFilesAccessor = _: persistentFiles;
+}
